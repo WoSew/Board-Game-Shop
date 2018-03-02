5b6e496865e5d825aaee3f5256578e0c2751aff0
WoSew <wojseweryn@gmail.com>
2018-03-02 21:11:31 +0100

    Created project

---
+++ BoardGameShopMVC/BoardGameShopMVC/BoardGameShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardGameShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
