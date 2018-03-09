e4fd2503be9c8bef947851abf50d79e25dc256c8
WoSew <wojseweryn@gmail.com>
2018-03-09 19:14:08 +0100

    Index.mobile

--- BoardGameShopMVC/BoardGameShopMVC/BoardGameShopMVC/DAL/StoreInitializer.cs
+++ BoardGameShopMVC/BoardGameShopMVC/BoardGameShopMVC/DAL/StoreInitializer.cs
@@ -39,15 +39,15 @@ namespace BoardGameShopMVC.DAL
 
             var games = new List<Game>
             {
-                new Game() { GameID = 1, DesignerName = "Antoine Bauza", GameTitle = "7 Wonders", Price = 129.99M, GameFileName = "7wonder.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 9},
+                new Game() { GameID = 1, DesignerName = "Antoine Bauza", GameTitle = "7 Wonders", Price = 129.99M, GameFileName = "7wonders.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 9},
                 new Game() { GameID = 2, DesignerName = "Eric Lange", GameTitle = "Blood Rage", Price = 259.99M, GameFileName = "bloodRage.png", IsBestseller = true, DateAdded = new DateTime(2018, 03, 05), CategoryId = 10},
                 new Game() { GameID = 3, DesignerName = "Jean-Louis Roubira", GameTitle = "Dixit", Price = 89.99M, GameFileName = "dixit.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 4},
                 new Game() { GameID = 4, DesignerName = "Corey Konieczka, Nikki Valens", GameTitle = "Eldritch Horror", Price = 299.99M, GameFileName = "eldritchHorror.png", IsBestseller = true, DateAdded = new DateTime(2018, 03, 05), CategoryId = 3},
                 new Game() { GameID = 5, DesignerName = "Bruno Cathala", GameTitle = "Five Tribes", Price = 149.99M , GameFileName = "fiveTribes.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 2},
-                new Game() { GameID = 6, DesignerName = "Nikki Valens", GameTitle = "Mansions of Madness", Price = 359.99M, GameFileName = "mansionsOfMadness.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 3},
+                new Game() { GameID = 6, DesignerName = "Nikki Valens", GameTitle = "Mansions of Madness", Price = 359.99M, GameFileName = "mansionsOfMadness.png", IsBestseller = false, DateAdded = new DateTime(2017, 01, 01), CategoryId = 3},
                 new Game() { GameID = 7, DesignerName = "Uwe Rosenberg", GameTitle = "Patchwork", Price = 59.99M, GameFileName = "patchwork.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 7},
-                new Game() { GameID = 8, DesignerName = "Philippe Keyaerts", GameTitle = "Smallwordl", Price = 149.99M, GameFileName = "smallworld.png", IsBestseller = true, DateAdded = new DateTime(2018, 03, 05), CategoryId = 9},
-                new Game() { GameID = 9, DesignerName = "Colby Dauch", GameTitle = "Summoner Wars", Price = 159.99M , GameFileName = "summonerWars.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 05), CategoryId = 5}
+                new Game() { GameID = 8, DesignerName = "Philippe Keyaerts", GameTitle = "Smallwordl", Price = 149.99M, GameFileName = "smallworld.png", IsBestseller = true, DateAdded = new DateTime(2018, 03, 03), CategoryId = 9},
+                new Game() { GameID = 9, DesignerName = "Colby Dauch", GameTitle = "Summoner Wars", Price = 159.99M , GameFileName = "summonerWars.png", IsBestseller = false, DateAdded = new DateTime(2018, 03, 01), CategoryId = 5}
             };
 
             games.ForEach(x => context.Games.AddOrUpdate(x));
