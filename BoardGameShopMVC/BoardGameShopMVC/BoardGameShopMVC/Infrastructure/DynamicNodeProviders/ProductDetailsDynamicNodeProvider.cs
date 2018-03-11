78d0ad4d3aaea967e158febb2897069ae8cf4936
WoSew <wojseweryn@gmail.com>
2018-03-11 11:44:14 +0100

    Site map, created dynamic node prividers

---
+++ BoardGameShopMVC/BoardGameShopMVC/BoardGameShopMVC/Infrastructure/DynamicNodeProviders/ProductDetailsDynamicNodeProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BoardGameShopMVC.DAL;
+using BoardGameShopMVC.Models;
+using MvcSiteMapProvider;
+
+namespace BoardGameShopMVC.Infrastructure.DynamicNodeProviders
+{
+    public class ProductDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext();
+
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach (Game game in db.Games)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = game.GameTitle;
+                n.Key = "Game_" + game.GameID;
+                n.ParentKey = "Category_" + game.CategoryId;
+                n.RouteValues.Add("id", game.GameID);
+                returnValue.Add(n);
+            }
+            return returnValue;
+        }
+    }
+}
---
+++ BoardGameShopMVC/BoardGameShopMVC/BoardGameShopMVC/Infrastructure/DynamicNodeProviders/ProductListDynamicNodeProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using BoardGameShopMVC.DAL;
+using BoardGameShopMVC.Models;
+using MvcSiteMapProvider;
+
+namespace BoardGameShopMVC.Infrastructure.DynamicNodeProviders
+{
+    public class ProductListDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach(Category category in db.Categories)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = category.Name;
+                n.Key = "Category_" + category.CategoryId;
+                n.RouteValues.Add("categoryName", category.Name);
+                returnValue.Add(n);
+            }
+            return returnValue;
+        }
+    }
+}
